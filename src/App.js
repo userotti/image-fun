ce96e583d3ebf4018d94d25f3b21eccbafc13d67
Christian Frauenstein <christian@Christians-MacBook-Air.local>
2022-10-12 17:05:25 +0200

    feat: added the basic form components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { useState } from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import IconButton from '@mui/material/IconButton';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import SearchIcon from '@mui/icons-material/Search';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
 
-function App() {
+export default function App() {
+
+  const [searchText, setSearchText] = useState('')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="lg" sx={{
+          p: 2,
+          backgroundColor: '#fafafa',
+        }}>
+        <form 
+          onSubmit={(event)=>{
+            event.preventDefault()
+            console.log("emlk")
+          }}
+          style={{  
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center'}}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <FormControl size={'small'} sx={{ m: 1, width: '25ch', backgroundColor: '#fff', }} variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-search">Search for images...</InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-search"
+              value={searchText}
+              onChange={(event)=>{
+                setSearchText(event.target.value)
+              }}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    edge="end"
+                  >
+                    <SearchIcon />
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Search for images..."
+            />
+          </FormControl>
+          <Button type="submit" variant="contained" size="medium">Go</Button>
+        </form>
+      </Container>
+      <Container maxWidth="lg" sx={{
+          p: 2,
+          backgroundColor: '#fafafa',
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}>
+        
+      </Container>
+    </React.Fragment>
   );
 }
-
-export default App;
